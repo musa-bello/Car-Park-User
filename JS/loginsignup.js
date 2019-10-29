9320c925dc8eb10183ce5f95b05d7f494b7d15b0
charlene04 <charlesugbana04@gmail.com>
2019-10-29 07:06:31 +0100

    added javascript form validation for the sign up form

--- JS/loginsignup.js
+++ JS/loginsignup.js
@@ -34,3 +34,57 @@ signInContainer.style.width="100%";
 });
 
 
+
+
+
+
+
+function checkForm(){
+  var username = document.forms["RegForm"]["username"];
+  var email = document.forms["RegForm"]["email"];
+  var pword = document.forms["RegForm"]["pword"];
+
+  if(username.value== ""){
+    window.alert("Please enter your name.");
+    username.focus();
+    return false;
+  }
+  re= /^\w+$/;
+  if(!re.test(username.value)){
+    window.alert("Your name must contain only letters, numbers and underscore.");
+    username.focus;
+    return false;
+  }
+  if(pword.value != ""){
+    if(pword.value.length < 6){
+      window.alert("Your password must not be less than six characters.");
+      pword.focus();
+      return false; 
+    }
+    if(pword.value==username.value){
+      window.alert("Your password and Username must not be the same.")
+      pword.focus();
+      return false;
+    }
+    re= /[0-9]/;
+    if(!re.test(pword.value)){
+      window.alert("Password should contain at least one number(0-9).")
+      pword.focus();
+      return false;
+    }
+    re= /[a-z]/;
+    if(!re.test(pword.value)){
+      window.alert("Password should contain at least one lowercase letter(a-z).")
+      pword.focus();
+      return false;
+    }
+    re= /[A-Z]/;
+    if(!re.test(pword.value)){
+      window.alert("Password should contain at least one uppercase letter(A-Z).")
+      pword.focus();
+      return false;
+  }
+
+  return true
+}
+}
