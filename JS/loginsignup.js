7710b433caa51fa56f6788865697e6ad66919f3a
charlene04 <charlesugbana04@gmail.com>
2019-10-29 04:13:03 +0100

    FE outlook

--- JS/loginsignup.js
+++ JS/loginsignup.js
@@ -32,3 +32,55 @@ signInContainer.style.opacity="1";
 signInContainer.style.width="100%";
 
 });
+
+
+function checkForm(form){
+  if(form.name.value == ""){
+    alert("Error: Username cannot be blank!");
+    form.name.focus();
+    return false;
+  }
+  re = /^\w+$/;
+  if(!re.test(form.username.value)){
+    alert("Error: Username must contain only letters!");
+    form.username.focus();
+    return false;
+  }
+  re = /[0-9]/;
+    if(!re.test(form.username.value)){
+      alert("Error: Your biological name cannot contain numbbers!");
+      form.reg-pword.focus();
+      return false;
+    }
+  if(form.reg-pword.value != ""){
+    if(form.reg-pword.value.lenght < 6){
+      alert("Error: Password must contain at least six characters!");
+      form.reg-pword.focus();
+      return false;
+    }
+    if(form.reg-pword.value == form.username.value){
+      alert("Error: Password must be different from username!");
+      form.reg-pword.focus();
+      return false;
+    }
+    re = /[0-9]/;
+    if(!re.test(form.reg-pword.value)){
+      alert("Error: Password must contain atleast one number (0-9)!");
+      form.reg-pword.focus();
+      return false;
+    }
+    re = /[a-z]/;
+    if(!re.test(form.reg-pword.value)){
+      alert("Error: Password must contain atleast one lowercase letter (a-z)!");
+      form.reg-pword.focus();
+      return false;
+    }
+    re = /[A-Z]/;
+    if(!re.test(form.reg-pword.value)){
+      alert("Error: Password must contain atleast one uppercase letter (A-Z)!");
+      form.reg-pword.focus();
+      return false;
+    }
+  }
+  alert("Registration Successful!")
+}
