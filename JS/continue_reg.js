70a37a1f96454208a8eeceb010d815742db7d59e
charlene04 <charlesugbana04@gmail.com>
2019-11-10 01:09:03 +0100

    updates

--- JS/continue_reg.js
+++ JS/continue_reg.js
@@ -7,6 +7,7 @@ document.getElementById("addCar").addEventListener("click", function(){
     document.getElementById("first").style.display="block";
     document.getElementById("second").style.display="none";
     document.getElementById("third").style.display="none";
+    
 });
 document.getElementById("uploadCar").addEventListener("click", function(){
     document.getElementById("home").style.display="none";
@@ -34,3 +35,23 @@ document.getElementById("third1").addEventListener("click", function(){
     document.getElementById("second").style.display="block";
     document.getElementById("third").style.display="none";
 });
+
+
+function moveForward(){
+    document.getElementById("home").style.display="block";
+    document.getElementById("first").style.display="none";
+    document.getElementById("addCar").classList.remove('fa-chevron-right');
+    document.getElementById("addCar").classList.add('fa-check');
+}
+function moveForward2(){
+    document.getElementById("home").style.display="block";
+    document.getElementById("second").style.display="none";
+    document.getElementById("uplaodCar").classList.remove('fa-chevron-right');
+    document.getElementById("uplaodCar").classList.add('fa-check');
+}
+function moveForward3(){
+    document.getElementById("home").style.display="block";
+    document.getElementById("third").style.display="none";
+    document.getElementById("parkOwner").classList.remove('fa-chevron-right');
+    document.getElementById("parkOwner").classList.add('fa-check');
+}
